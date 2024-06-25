3c0a9af85892a3e3b623658a942782a8b0f83a50
Ferrisx4 <ferrisx4@gmail.com>
2024-06-25 02:51:33 +0000

    Start to increase functionality

--- titlesetter.js
+++ titlesetter.js
@@ -1,9 +1,25 @@
 function set_title(title)
 {
+    // Set the document title.
     document.title=title;
 
+    // Set the color of the entire page.
     var color = stringToColour(title);
     document.body.style.backgroundColor = color;
+
+    // Set/update the URL parameter.
+    var currentURL = window.location.href;
+    console.log(currentURL);
+
+    // window.location.href = currentURL + title;
+}
+
+/*
+Upon page reload, after browser/tab refresh, or whenever, get the "title" URL parameter
+and set that as the title.
+*/
+function restore_title() {
+
 }
 
 // Code to change the background color based on the string.
